553c0bef10ec35248b9fcd4e2c605e3a8335bd26
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-23 15:13:33 -0400

    Admin_API: validação de login e senha ...

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const user = {
+  login: document.getElementById('login'),
+  password: document.getElementById('password'),
+
   submit(event){
     event.preventDefault();
-  }
+    try {
+      user.validate()
+    } catch (error) {
+      alert(error.message)
+    }
+  },
+  validate(){
+    if(user.login.value === "",
+       user.password.value === ""){
+       throw new Error("Por favor, preencha todos os campos")
+    }
+    if(user.login.value.length < 4){
+      throw new Error("Por favor, preencha todos os campos")
+
+    }
+  }  
 }
+
